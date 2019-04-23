b30e9d5f32953fddbc74109debfd8e9a2140b726
Gursahib Singh <gursahib.kvm@gmail.com>
2019-04-23 08:38:35 +0530

    Dunder(Double UNDERscore) methods

---
+++ dunderMethods.py
@@ -0,0 +1,15 @@
+class Vector:
+    def __init__(self,*coeffs):
+        self.coeffs=coeffs 
+    def __repr__(self):
+        return 'Vector(*{!r})'.format(self.coeffs)
+    def __add__(self,other):
+        return Vector(*(x+y for x,y in zip(self.coeffs,other.coeffs))) #Vector object is returned
+    def __len__(self):
+         return len(self.coeffs)
+    def __call__(self,*coeffs):
+          self.coeffs=coeffs 
+P1=Vector(1,2,3) 
+P2=Vector(2,2,4) 
+P3=P1+P2
+
